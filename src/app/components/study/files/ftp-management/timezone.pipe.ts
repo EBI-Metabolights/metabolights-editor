c947f7b4d31ea0a2a516f1f0b0db71c9f314a3ae
cmartin <cmartin@ebi.ac.uk>
2022-11-08 12:05:00 +0000

    comitting these tests and mocks for now as we have other priorities

--- src/app/components/study/files/ftp-management/timezone.pipe.ts
+++ src/app/components/study/files/ftp-management/timezone.pipe.ts
@@ -5,9 +5,8 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class TimezoneTransformPipe implements PipeTransform {
 
     transform(serverTimestamp:string) {
-      console.log(' time ' + serverTimestamp);
-      if (serverTimestamp ===  'N/A' || serverTimestamp ===  '...') {
-        return  '';
+      if (serverTimestamp ===  'N/A' || serverTimestamp ===  '...' || serverTimestamp === 'NONE' || serverTimestamp === '') {
+        return  'N/A';
       }
       return new Date(
             new Date(serverTimestamp).getTime() + (new Date().getTimezoneOffset() * 60 * 1000)
