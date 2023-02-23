17674a4d4a62c47b843fdce73045f8a622d5ccac
ozgur <ozgur@ebi.ac.uk>
2023-02-23 17:09:23 +0000

    Resctict multiple study creation

--- src/app/services/error/bad-input.ts
+++ src/app/services/error/bad-input.ts
@@ -2,12 +2,13 @@ import * as toastr from "toastr";
 
 export class BadInput {
   constructor(error: any) {
-    const errorMessage = error.message
-      ? error.message
+    const errorMessage = error.error.message
+      ? error.error.message
+      : error.message ? error.message
       : "Bad request input error";
     const errorStatus = error.statusText ? error.statusText : "";
     toastr.warning(errorMessage, errorStatus, {
-      timeOut: "1000",
+      timeOut: "5000",
       positionClass: "toast-top-center",
       preventDuplicates: true,
       extendedTimeOut: 0,
