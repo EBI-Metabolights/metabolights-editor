4a9da872d63e8f2b5121d646e1eff1d046521ca7
ozgur <ozgur@ebi.ac.uk>
2023-12-15 19:31:51 +0000

    environment name definition update

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -35,7 +35,7 @@ export class LoginComponent implements OnInit {
   ) {
     this.baseHref = this.configService.baseHref;
     this.environmentName = this.platformLocation.getBaseHrefFromDOM();
-    this.environmentName = this.environmentName.replace("/metabolights/", "");
+    this.environmentName = this.environmentName.replace("/metabolights", "");
     this.environmentName = this.environmentName.replace("/editor", "");
     this.environmentName = this.environmentName.replace("/", "");
 
