ed596a4be6e7bb7133978635f3fb9bc3fdb99be5
Felix Amaladoss <famaladoss@ebi.ac.uk>
2024-09-12 15:43:17 +0100

    Add newstate flag for dev

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,8 @@
 // This is the environent for the development server, not for local development - that environment file is
 // environment.ts
 export const environment = {
-  production: false,
-  context: "development",
+  production: true,
+  context: "production",
   isTesting: false,
+  useNewState: true
 };
