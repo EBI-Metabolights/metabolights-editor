78604fea452db5ba6e2afae6349a8a138d6e9516
ozgury <ozgury@ebi.ac.uk>
2025-01-07 10:05:22 +0000

    fix: check reviewCode parameter first in header interceptor

--- src/app/services/interceptors/header.interceptor.ts
+++ src/app/services/interceptors/header.interceptor.ts
@@ -1,30 +1,27 @@
 import { inject, Injectable } from "@angular/core";
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from "@angular/common/http";
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from "@angular/common/http";
 import { Observable } from "rxjs";
 import { disambiguateUserObj } from "../editor.service";
 import { ConfigurationService } from "src/app/configuration.service";
 import { Store } from "@ngxs/store";
-import { environment } from "src/environments/environment";
-import { ApplicationState } from "src/app/ngxs-store/non-study/application/application.state";
 import { StudyPermission } from "../headers";
+import { ActivatedRoute} from '@angular/router';
 /* eslint-disable @typescript-eslint/naming-convention */
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
 
-  studyPermission$: Observable<StudyPermission> = inject(Store).select(ApplicationState.studyPermission);
-
-  studyPermission:StudyPermission = null;
+  studyPermission: StudyPermission = null;
   constructor(
     private configService: ConfigurationService,
-    private store: Store) {
-      this.ngOnInit()
-    }
-
-  ngOnInit() {
-      this.studyPermission$.subscribe((value) => {
-        this.studyPermission = value;
-      });
-    }
+    private activatedRoute: ActivatedRoute,
+    private store: Store
+  ) {
+  }
 
   intercept(
     request: HttpRequest<unknown>,
@@ -32,55 +29,55 @@ export class HeaderInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<unknown>> {
     const endpoint = this.configService?.config?.endpoint;
     const origin = this.configService.config?.origin;
-    const targetUrl = origin + request.url ;
-    if (request.url.startsWith(endpoint) || targetUrl.startsWith(endpoint)){
-      let userToken = localStorage.getItem("userToken");
-    const user = this.getUserObject()
-    if(userToken === null) {
-      if ( user !== null) {
-        userToken = disambiguateUserObj(this.getUserObject());
-        localStorage.setItem("userToken", userToken);
-      }
-    }
-    if (userToken !== null){
-      request = request.clone({
-        setHeaders: {
-          user_token: userToken,
-        },
-      });
-    }else {
-      request = request.clone({
-        setHeaders: {
-          user_token: "",
-        },
-      });
-    }
-    const jwt = localStorage.getItem("jwt");
-    if (jwt !== null) {
-      request = request.clone({
-        setHeaders: {
-          Authorization: "Bearer " + jwt
-        },
-      });
-    }
-    const permissions = this.studyPermission;
-    if (permissions && permissions.obfuscationCode && ["INREVIEW", "INCURATION"].includes(permissions.studyStatus.toUpperCase())){
-      if (permissions.userName === null || permissions.userName.length == 0) {
-        request = request.clone({
-          setHeaders: {
-            "Obfuscation-Code": permissions.obfuscationCode
+    const targetUrl = origin + request.url;
+    if (request.url.startsWith(endpoint) || targetUrl.startsWith(endpoint)) {
+      const reviewCode = this.activatedRoute.snapshot.queryParams["reviewCode"];
+      if ( reviewCode ) {
+          request = request.clone({
+            setHeaders: {
+              "Obfuscation-Code": reviewCode,
+            },
+          });
+      } else
+      {
+        let userToken = localStorage.getItem("userToken");
+        const user = this.getUserObject();
+        if (userToken === null) {
+          if (user !== null) {
+            userToken = disambiguateUserObj(this.getUserObject());
+            localStorage.setItem("userToken", userToken);
           }
-        });
+        }
+        if (userToken !== null) {
+          request = request.clone({
+            setHeaders: {
+              "User-Token": userToken,
+            },
+          });
+        } else {
+          request = request.clone({
+            setHeaders: {
+              "User-Token": "",
+            },
+          });
+        }
+        const jwt = localStorage.getItem("jwt");
+        if (jwt !== null) {
+          request = request.clone({
+            setHeaders: {
+              Authorization: "Bearer " + jwt,
+            },
+          });
+        }
       }
-    }
+
     }
     return next.handle(request);
   }
 
   getUserObject() {
     const user = localStorage.getItem("user");
-    if (user !== null)
-      return JSON.parse(user);
-    return null
+    if (user !== null) return JSON.parse(user);
+    return null;
   }
 }
